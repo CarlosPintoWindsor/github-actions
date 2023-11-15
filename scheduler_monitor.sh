1a73c2b8e20bce1ee340bf61a804896ae1cb0d9f
CarlosPinto <carlosp@windsorstore.com>
2023-11-15 14:58:19 -0500

    Trigger Katalon Scheduler

--- scheduler_monitor.sh
+++ scheduler_monitor.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 TEST_RUN=${1}
+USER_CREDS=${2}
 echo "Monitoring scheduled Run test $TEST_RUN"
+echo Trigger Http Rest call
+
+get_schedule_exec_details 
+
+
+
+get_schedule_exec_details()(
+
+    local SCHEDULE_ID=${1}
+
+    local HTTP_RESPONSE= $(curl --request GET \
+     --url https://testops.katalon.io/api/v1/smart-scheduler/$TEST_RUN \
+     --header 'accept: */*' \
+     --header "authorization: Basic $USER_CREDS" ` | jq '.' )
+    
+    echo $HTTP_RESPONSE
+)
