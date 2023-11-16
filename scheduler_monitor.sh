8b6dcd87114f07dd9668101885b2e3c19b520402
CarlosPinto <carlosp@windsorstore.com>
2023-11-15 20:10:36 -0500

    Trigger Katalon Scheduler

--- scheduler_monitor.sh
+++ scheduler_monitor.sh
@@ -11,20 +11,20 @@ get_schedule_exec_details()(
     local HTTP_RESPONSE=$(curl --request GET --url https://testops.katalon.io/api/v1/smart-scheduler/$TEST_RUN --header 'accept: */*' --header "authorization: Basic $USER_CREDS"  | jq '.' )
     #echo $HTTP_RESPONSE
     local RUNNING_CONFIG=$(echo $HTTP_RESPONSE | jq '.runConfiguration' )
-    local SCHEDULE_NAME=$(echo $RUNNING_CONFIG | jq '.name')
-    local SCHEDULE_ID=$(echo $RUNNING_CONFIG | jq '.id')
+    local SCHEDULE_NAME=$(echo $RUNNING_CONFIG | jq -r '.name')
+    local SCHEDULE_ID=$(echo $RUNNING_CONFIG | jq -r '.id')
     local LATEST_JOB=$(echo $RUNNING_CONFIG | jq '.latestJob' )
-    local JOB_ID=$(echo $LATEST_JOB | jq '.id')
-    local JOB_STATUS=$(echo $LATEST_JOB | jq '.status')
-    local EXECUTION_ID=$(echo $LATEST_JOB | jq '.execution.order')
-    local JOB_ORDER=$(echo $LATEST_JOB | jq 'order')
-    local QUEUED_AT=$(echo $LATEST_JOB | jq '.queuedAt')
-    local STARTED_AT=$(echo $LATEST_JOB | jq '.startTime')
-    local STOPPED_AT=$(echo $LATEST_JOB | jq '.stopTime')
+    local JOB_ID=$(echo $LATEST_JOB | jq -r '.id')
+    local JOB_STATUS=$(echo $LATEST_JOB | jq -r '.status')
+    local EXECUTION_ID=$(echo $LATEST_JOB | jq -r '.execution.order')
+    local JOB_ORDER=$(echo $LATEST_JOB | jq  -r '.order')
+    local QUEUED_AT=$(echo $LATEST_JOB | jq -r '.queuedAt')
+    local STARTED_AT=$(echo $LATEST_JOB | jq -r  '.startTime')
+    local STOPPED_AT=$(echo $LATEST_JOB | jq -r '.stopTime')
     echo "Schedule ID: $SCHEDULE_ID - Schedule Name: $SCHEDULE_NAME"
     echo "Latest job Id: $JOB_ID - Job Order: $JOB_ORDER -  Exec Id: $EXECUTION_ID - Job Status: $JOB_STATUS - Queued At: $QUEUED_AT - Started At: $STARTED_AT - Stopped At: $STOPPED_AT "
 
-    if [ "$JOB_STATUS" != "$SUCESS_STRING" ]; then
+    if [[ "$JOB_STATUS" != "$SUCESS_STRING" ]]; then
         echo Sleeping for 30 seconds
         sleep 30
         get_schedule_exec_details
